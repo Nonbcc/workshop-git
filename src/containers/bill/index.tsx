8eab3e9f2e2cf89b3e7ca545849e1cd62529d10f
PattySoCute <palita.sim@gmail.com>
2023-01-14 16:11:42 +0700

    third commit

--- src/containers/bill/index.tsx
+++ src/containers/bill/index.tsx
@@ -9,7 +9,7 @@ import {
   StyledWrapper,
   TitleNoMargin
 } from '@containers/bill/style'
-import { Col, Row, Table } from 'antd'
+import { Col, Input, Row, Table } from 'antd'
 import { ColumnsType } from 'antd/lib/table'
 import { ReactElement } from 'react'
 
@@ -45,20 +45,16 @@ const BillContainer = (): ReactElement => {
         <StyledSubHeaderDetail>
           <Row>
             <Col span={16}>
-              <TitleNoMargin level={5}>นาม</TitleNoMargin>
-              <TitleNoMargin level={5}>Name ________________________________________</TitleNoMargin>
+              <Input prefix={'นาม'} />
             </Col>
             <Col span={8}>
-              <TitleNoMargin level={5}>วันที่</TitleNoMargin>
-              <TitleNoMargin level={5}>Date _________________</TitleNoMargin>
+              <Input prefix={'วันที่'} />
             </Col>
             <Col span={16}>
-              <TitleNoMargin level={5}>ที่อยู่</TitleNoMargin>
-              <TitleNoMargin level={5}>Address ______________________________________</TitleNoMargin>
+              <Input prefix={'ที่อยู่'} />
             </Col>
             <Col span={8}>
-              <TitleNoMargin level={5}>เลขผู้เสียภาษี</TitleNoMargin>
-              <TitleNoMargin level={5}>TaxId _________________</TitleNoMargin>
+              <Input prefix={'เลขผู้เสียภาษี'} />
             </Col>
           </Row>
         </StyledSubHeaderDetail>
